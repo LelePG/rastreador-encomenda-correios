be83af4ddf4fa67f292773ea1ec7a039e7d87ff9
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2021-07-05 23:55:05 -0300

    Refatoração do servidor

--- back/server.js
+++ back/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.use(express.static('../front'))// provê para a aplicação todos os arquivos estáticos no diretório .
-app.get('/teste', (req, res) => {
-  res.send('Tô funcionando!')
+app.use(express.static(__dirname+ '/../front'))// provê para a aplicação todos os arquivos estáticos no diretório .
+app.get('/', (req, res) => {
+  res.send('ok')
 })
 
 app.listen(port, () => {
